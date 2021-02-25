9ec2a7ee9db04831ec202c66736ac7f740fb7990
Helen Amanuel <hamanuel@humana.com>
2021-02-25 13:37:04 -0500

    latest with comments

--- src/requests.js
+++ src/requests.js
@@ -32,13 +32,42 @@ function checkDirectReports(directReports) {
     )
       .then((res) => res.json())
       .then((res) => {
-        if (res[0].directReports) {
-          console.log(
-            `need to fetch again and find other direct reports for ${el.name}`
-          );
-        } else {
-          console.log(`${el.name} doesnt have direct reports`);
+//produces an array that holds associates that have direct reports 
+//manager = someone who has direct reports beneath them
+//[cory, justin]
+       var peoplewithdirectreportsArray =  res.filter(el => {//TODO:
+            el[0].directReports
+        })
+
+        directReports.push(otherfolkes)
+//if this array is not nil
+    if(peoplewithdirectreportsArray)
+    //for each person who has a direct repot puash their information into our direct report array
+        peoplewithdirectreportsArray.forEach(
+            directReports.push(
+               { manager.name,
+                manager.email,
+                manager.tsoId
+               }
+            )
+            //take their tsoid and feed it to our top function//TODO:
+            getdirectReports(manager.tsoId)
+        )else {
+            console.log("process is complete you have all the emails")//TODO:
         }
+
+        // if (res[0].directReports) {
+        //   console.log(
+        //     `need to fetch again and find other direct reports for ${el.name}`
+        //   );
+        //   directReports.push({
+        //     name: el.name,
+        //     email: el.email,
+        //     tsoId: el.tsoId
+        //   });
+        // } else {
+        //   console.log(`${el.name} doesnt have direct reports`);
+        // }
       });
   });
 }
